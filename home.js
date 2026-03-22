f2d9589414f925c5948468460612ca9b250f8c60
Rohitraam777-Sri <rohitraam441@gmail.com>
2026-03-22 15:11:16 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,13 @@
+ var side = document.querySelector(".side")
+ side.style.display = "none"
+
+ function Showside()
+ {
+    side.style.display="block"
+ }
+
+ function close()
+ {
+     side.style.display="none"
+
+ }
